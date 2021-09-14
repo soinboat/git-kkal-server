8369b3a67a77bddd61f4b29695bfd8d00c9c25fe
seunghyun oh <oshyun6623@gmail.com>
2021-09-14 18:29:41 +0900

    refactor: remove flat method from lineData

--- utils/lineDataGenerator.js
+++ utils/lineDataGenerator.js
@@ -5,35 +5,31 @@ const lineDataGenerator = (logListData) => {
     logListObject[log.hash] = log;
   });
 
-  const lineData = logListData
-    .map((node, index) => {
-      const from = [node.position, index];
-      const rawLineData = node.parents.map((parent, _, parentsClone) => {
-        const parentNode = logListObject[parent];
-        const to = [parentNode.position, parentNode.index];
-        const color =
-          node.position > parentNode.position ? node.color : parentNode.color;
-        const singleLineData = { color, points: [from] };
-        if (parentNode.position > node.position) {
-          const checkPoint = [to[0], from[1] + SLOPE];
+  const lineData = logListData.map((node, index) => {
+    const from = [node.position, index];
+    const rawLineData = node.parents.map((parent, _, parentsClone) => {
+      const parentNode = logListObject[parent];
+      const to = [parentNode.position, parentNode.index];
+      const color =
+        node.position > parentNode.position ? node.color : parentNode.color;
+      const singleLineData = { color, points: [from] };
+      if (parentNode.position > node.position) {
+        const checkPoint = [to[0], from[1] + SLOPE];
+        singleLineData.points.push(checkPoint);
+      } else if (parentNode.position < node.position) {
+        if (parentsClone.length === 1) {
+          const checkPoint = [from[0], to[1] - SLOPE];
           singleLineData.points.push(checkPoint);
-        } else if (parentNode.position < node.position) {
-          if (parentsClone.length === 1) {
-            const checkPoint = [from[0], to[1] - SLOPE];
-            singleLineData.points.push(checkPoint);
-          } else {
-            singleLineData.color =
-              node.position < parentNode.position
-                ? node.color
-                : parentNode.color;
-          }
+        } else {
+          singleLineData.color =
+            node.position < parentNode.position ? node.color : parentNode.color;
         }
-        singleLineData.points.push(to);
-        return singleLineData;
-      });
-      return rawLineData;
-    })
-    .flat();
+      }
+      singleLineData.points.push(to);
+      return singleLineData;
+    });
+    return rawLineData;
+  });
 
   return lineData;
 };
